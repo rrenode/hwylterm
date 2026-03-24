2069d2a60e66c8487c74c4fe22ffd904a3d1fe40
rrenode <robert.renodej@gmail.com>
2026-03-24 15:39:06 -0400

    add console determines effective color system

--- src/hwylterm/console.nim
+++ src/hwylterm/console.nim
@@ -1,27 +1,57 @@
 import ./rendermodes
+import std/[options]
 
 type
-  Console = object
+  Console = ref object
     file*: File
     bbMode: BbMode ## \
     ## The rendering mode used
-    colorSystem: ColorSystem ## \
+    ##colorSystem: ColorSystem ## \
     ## The color system used
+    forcedColorSystem: Option[ColorSystem] ## \
+    ## This value ignores all other checks except env `NO_COLOR"`
 
 proc determineEffectiveColorSystem(c: Console): ColorSystem = 
+  ## Determines the colorsystem for a console in priority of:
+  ##    1. env: `NO_COLOR`
+  ##    2. env: `HWYLTERM_NO_COLOR`
+  ##    3. env: `HWYLTERM_FORCE_COLOR` <- (within term capability)
+  ##    4..end based on capability from system APIs and convention envs
   let clrCapability = checkColorCapability(c.file)
   let clrPolicy = checkColorPolicy()
 
+  proc clampPolicyToCapability(cap , pol: ColorSystem): ColorSystem =
+    case pol:
+    of csBasic:
+      if cap in {csBasic, csEightBit, csTrueColor}: return pol
+    of csEightBit:
+      if cap in {csEightBit, csTrueColor}: return pol
+    of csTrueColor:
+      if cap == csTrueColor: return pol
+    else:
+      discard
+    return cap
+
+  if c.forcedColorSystem.isSome(): return c.forcedColorSystem.get()
+  case clrPolicy
+  of csAuto: return clrCapability
+  of csNone: return csNone
+  else: return clampPolicyToCapability(clrCapability, clrPolicy)
+
 proc determineEffectiveBbMode(c: Console): BbMode = 
-  discard
+  let bbCapability = checkBbCapability(c.file)
+  let checkBbPolicy = checkBbPolicy()
 
 proc newConsole*(file: File = stdout): Console =
+  new(result)
   result.file = file
-  result.colorSystem = result.determineEffectiveColorSystem()
   result.bbMode = result.determineEffectiveBbMode()
+
+proc forceColorSystem*(c: Console, clrSys: ColorSystem) =
+  c.forcedColorSystem = some(clrSys)
 
 proc bbMode*(c: Console): BbMode =
   c.bbMode
 
 proc colorSystem*(c: Console): ColorSystem =
-  c.colorSystem
+  c.determineEffectiveColorSystem()
